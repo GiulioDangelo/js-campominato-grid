6be9775b98dbd5a7f5cefde30b39ebaee06119ca
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-21 17:49:57 +0200

    add if

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const startBtn = document.querySelector(".start");
 
 startBtn.addEventListener("click", function () {
+    
     const eleGrid = document.querySelector(".grid");
+    const GridWidth = eleGrid.clientWidth
+    console.log(GridWidth)
     
-	eleGrid.innerHTML = "";
-	for (let i = 1; i <= 100; i++) {
-        eleGrid.innerHTML += `<div class="cell">${i}</div>`;
+	const level = document.querySelector("#level").value;
+	if (level == "easy") {
+		cellWidth = GridWidth / 7;
+
+	} else if (level == "mid") {
+		cellWidth = GridWidth / 8;
+
+	} else if (level == "hard") {
+		cellWidth = GridWidth / 10;
 	}
-    
+
+	eleGrid.innerHTML = "";
+	for (let i = 1; i <= cellWidth; i++) {
+		eleGrid.innerHTML += `<div class="cell">${i}</div>`;
+	}
+
 	const listCell = document.querySelectorAll(".cell");
-    
+
 	for (let i = 0; i < listCell.length; i++) {
-        const cell = listCell[i];
+		const cell = listCell[i];
 		cell.addEventListener("click", colorCell);
 	}
-    
-
-    const level = document.querySelector('#level').value
-    console.log(level)
-    
 });
 
 
-function colorCell() {
-    this.classList.toggle("clicked");
-}
 
+
+
+function colorCell() {
+	this.classList.toggle("clicked");
+}
