0aa820492edea917203b10c9616eddaf65ec369e
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-21 17:06:14 +0200

    add select

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-/*const startBtn = document.querySelector('.start')
+const startBtn = document.querySelector(".start");
 
-startBtn.addEventListener('click', function () {
-    eleGrid.classList.toggle('invisible')
-})
-*/
+startBtn.addEventListener("click", function () {
+    const eleGrid = document.querySelector(".grid");
+    
+	eleGrid.innerHTML = "";
+	for (let i = 1; i <= 100; i++) {
+        eleGrid.innerHTML += `<div class="cell">${i}</div>`;
+	}
+    
+	const listCell = document.querySelectorAll(".cell");
+    
+	for (let i = 0; i < listCell.length; i++) {
+        const cell = listCell[i];
+		cell.addEventListener("click", colorCell);
+	}
+    
 
-const eleGrid = document.querySelector('.grid')
-
-for (let i = 1; i <= 100; i++) {
-    eleGrid.innerHTML += `<div class="cell">${i}</div>`
-}
-
-
-const listCell = document.querySelectorAll('.cell')
-
-for (let i = 0; i < listCell.length; i++) {
-    const cell = listCell[i];
-    cell.addEventListener('click', colorCell)
-}
+    const level = document.querySelector('#level').value
+    console.log(level)
+    
+});
 
 
 function colorCell() {
-    this.classList.toggle('clicked')
+    this.classList.toggle("clicked");
 }
 
-/*function invisible() {
-    eleGrid.classList.toggle('invisible')
-}*/
